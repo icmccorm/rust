840f9dc85635c472c375bd5e6e4e5e8d8133d681
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-12-10 20:53:13 +0000

    Fixed GEP semantics.

--- src/tools/miri/src/shims/llvm/hooks/intptr.rs
+++ src/tools/miri/src/shims/llvm/hooks/intptr.rs
@@ -78,48 +78,30 @@ fn miri_get_element_pointer_result<'tcx>(
 ) -> InterpResult<'tcx, Pointer<Option<crate::Provenance>>> {
     let this = ctx.eval_context_mut();
     let (ptr_offset, _) = base_ptr.overflowing_offset(offset, this);
+    debug!("GEP - {} to {}", base_ptr.addr().bytes(), ptr_offset.addr().bytes());
     if let Some(Concrete { alloc_id, tag }) = base_ptr.provenance {
         let (size, _, _) = this.get_alloc_info(alloc_id);
         let base_address = intptrcast::GlobalStateInner::alloc_base_addr(this, alloc_id)?;
         let addr_upper_bound = base_address.checked_add(size.bytes());
-        if let Some(addr_upper_bound) = addr_upper_bound {
-            if ptr_offset.addr().bytes() >= addr_upper_bound {
-                let provenance = intptrcast::GlobalStateInner::alloc_id_from_addr(
-                    this,
-                    ptr_offset.addr().bytes(),
-                )
-                .and_then(|offset_alloc_id| {
-                    let alloc_map = this.memory.alloc_map();
-                    let alloc1 = alloc_map.get(alloc_id);
-                    let alloc2 = alloc_map.get(offset_alloc_id);
-                    alloc1.and_then(|a1| alloc2.map(|a2| (a1.0, a2.0)))
-                })
-                .and_then(|pair| {
-                    /*  It's undefined behavior dereference a pointer produced by GEP if that pointer is outside
-                     *  of the bounds of the original allocation. Additionally, we do not want to allow use of GEP
-                     *  to offset from a non-stack allocation into a stack allocation. So, if any of these cases
-                     *  apply, we keep the provenance of the base pointer, which will make it so that the pointer we
-                     *  produce will cause an access-out-of-bounds error when dereferenced.
-                     */
-                    let (base_mem_type, offset_mem_type) = pair;
-                    if this.machine.lli_config.gep_strict
-                        || (matches!(base_mem_type, MemoryKind::Machine(MiriMemoryKind::LLVMStack))
-                            || matches!(
-                                base_mem_type,
-                                MemoryKind::Machine(MiriMemoryKind::LLVMStack)
-                            )
-                            || base_mem_type != offset_mem_type)
-                    {
-                        base_ptr.provenance.map(|p| Ok(p))
-                    } else {
-                        Some(
-                            intptrcast::GlobalStateInner::expose_ptr(this, alloc_id, tag)
-                                .map(|_| Wildcard),
-                        )
-                    }
-                })
-                .map_or(Ok(None), |r| r.map(Some))?;
-                return Ok(Pointer::new(provenance, ptr_offset.addr()));
+        debug!("GEP FROM - ({})", alloc_id.0);
+        if let Some(source_allocation) = this.memory.alloc_map().get(alloc_id) {
+            if let Some(addr_upper_bound) = addr_upper_bound {
+                if ptr_offset.addr().bytes() >= addr_upper_bound {
+                    let source_allocation_kind = source_allocation.0;
+                    let provenance = if matches!(source_allocation_kind, MemoryKind::Machine(MiriMemoryKind::LLVMStack)) 
+                        || matches!(
+                            source_allocation_kind,
+                            MemoryKind::Machine(MiriMemoryKind::LLVMStatic)
+                        ) {
+                            base_ptr.provenance
+                        }else{
+                            debug!("GEP EXP - Exposing provenance: {}", alloc_id.0);
+                            intptrcast::GlobalStateInner::expose_ptr(this, alloc_id, tag)?;
+                            Some(Wildcard)
+                        };
+                    debug!("GEP TO - ({:?})", provenance);
+                    return Ok(Pointer::new(provenance, ptr_offset.addr()));
+                }
             }
         }
     }
